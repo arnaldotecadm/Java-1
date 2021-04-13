5889e609c2be170e2d65f9219835616e70f5ca71
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-04-13 14:41:50 -0300

    Adição de butter knife; Pequena refatoração da classe de RegistroDeEventos.

--- app/src/main/java/br/com/pucgo/atividade/RegistroDeEventos.java
+++ app/src/main/java/br/com/pucgo/atividade/RegistroDeEventos.java
@@ -18,80 +18,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RegistroDeEventos extends AppCompatActivity {
 
-    EditText nomeEvento, dataEvento;
-    Button voltar, registrarEvento;
+    @BindView(R.id.nomeEvento)
+    EditText nomeEvento;
+    @BindView((R.id.dataEvento))
+    EditText dataEvento;
+    @BindView(R.id.btnVoltarEvento)
+    Button voltar;
+    @BindView(R.id.btnRegistrarEvento)
+    Button registrarEvento;
     DatePickerDialog.OnDateSetListener datePicker;
+    @BindView(R.id.eventos)
     ListView eventos;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro_de_eventos);
 
-        eventos = findViewById(R.id.eventos);
-        voltar = findViewById(R.id.btnVoltarEvento);
-        registrarEvento = findViewById(R.id.btnRegistrarEvento);
-        nomeEvento = findViewById(R.id.nomeEvento);
-        dataEvento = findViewById(R.id.dataEvento);
+        // Isso aqui faz a magica da ligacao entre componente da tela e as propriedades
+        ButterKnife.bind(this);
 
-
-        ArrayList<Evento> eventosRegistrados = new ArrayList<Evento>();
+        ArrayList<Evento> eventosRegistrados = new ArrayList<>();
         //eventosRegistrados.add(new Evento(1, "Aula 1", "01/02/2000"));
         //eventosRegistrados.add(new Evento(2, "Aula 2", "02/02/2000"));
         Adaptador adaptador = new Adaptador(this, R.layout.eventos, eventosRegistrados);
         eventos.setAdapter(adaptador);
 
-        registrarEvento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!nomeEvento.getText().toString().isEmpty() && !dataEvento.getText().toString().isEmpty()){
-                    int contador;
-                    if(eventosRegistrados.isEmpty()){contador = 1;}
-                    else{contador = eventosRegistrados.get(eventosRegistrados.size()-1).getContador()+1;}
-                    Evento evento = new Evento(contador, nomeEvento.getText().toString(), dataEvento.getText().toString());
-                    eventosRegistrados.add(evento);
-                }else{
-                    Toast.makeText(RegistroDeEventos.this, "Preencher dados do evento", Toast.LENGTH_SHORT).show();
-                }
+        /**
+         * Aqui podemos simplificar a chamada do metodo onClick
+         * visto que so possui um metodo que precisamos implementar
+         * o mesmo vale para os metodos que seguem esta logica
+         */
+        registrarEvento.setOnClickListener( (view) -> {
+            if(!nomeEvento.getText().toString().isEmpty() && !dataEvento.getText().toString().isEmpty()){
+                int contador;
+                if(eventosRegistrados.isEmpty()){contador = 1;}
+                else{contador = eventosRegistrados.get(eventosRegistrados.size()-1).getContador()+1;}
+                Evento evento = new Evento(contador, nomeEvento.getText().toString(), dataEvento.getText().toString());
+                eventosRegistrados.add(evento);
+            }else{
+                Toast.makeText(RegistroDeEventos.this, "Preencher dados do evento ", Toast.LENGTH_SHORT).show();
             }
         });
 
-        dataEvento.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                int dia, mes, ano;
-                dia = calendar.get(Calendar.DAY_OF_MONTH);
-                mes = calendar.get(Calendar.MONTH);
-                ano = calendar.get(Calendar.YEAR);
-                DatePickerDialog dialog = new DatePickerDialog(
-                        RegistroDeEventos.this,
-                        android.R.style.Theme_Holo_Dialog_MinWidth,
-                        datePicker, ano, mes, dia
-                );
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-                return true;
-            }
+        dataEvento.setOnLongClickListener((view) -> {
+            Calendar calendar = Calendar.getInstance();
+            int dia, mes, ano;
+            dia = calendar.get(Calendar.DAY_OF_MONTH);
+            mes = calendar.get(Calendar.MONTH);
+            ano = calendar.get(Calendar.YEAR);
+            DatePickerDialog dialog = new DatePickerDialog(
+                    RegistroDeEventos.this,
+                    android.R.style.Theme_Holo_Dialog_MinWidth,
+                    datePicker, ano, mes, dia
+            );
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+            return true;
         });
 
-        datePicker = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month += 1;
-                String dataString = dayOfMonth + "/" + month + "/" + year;
-                dataEvento.setText(dataString);
-            }
+        datePicker = (view, year, month, dayOfMonth) -> {
+            month += 1;
+            String dataString = dayOfMonth + "/" + month + "/" + year;
+            dataEvento.setText(dataString);
         };
 
-        voltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(RegistroDeEventos.this, Menu.class);
-                startActivity(i);
-                finish();
-            }
+        voltar.setOnClickListener(v -> {
+            Intent i = new Intent(RegistroDeEventos.this, Menu.class);
+            startActivity(i);
+            finish();
         });
 
 
